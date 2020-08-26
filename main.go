507d607a131e5d103843445e109c89f9c10b5d85
Alexandre Setta <alexsetta@gmail.com>
2020-08-26 07:30:20 -0300

    View log file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -16,19 +18,31 @@ func main() {
 		log.Fatal(err)
 	}
 	dir := filepath.Dir(ex)
-	file := flag.String("file", "log.txt", "Nome do arquivo de log")
+	file := flag.String("f", "log.txt", "Log file name")
+	view := flag.Bool("v", false, "View log file")
 	flag.Parse()
+
+	if *view {
+		b, err := ioutil.ReadFile(dir + "/" + *file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(b))
+		return
+	}
+
 	c, err := clipboard.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 	s := "[" + time.Now().Format("02/01/2006 15:04:05") + "] " + c + "\n"
 
-	f, err := os.OpenFile(dir+"/"+*file, os.O_APPEND|os.O_CREATE, 0644)
+	f, err := os.OpenFile(dir+"/"+*file, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+
 	_, err = f.WriteString(s)
 	if err != nil {
 		log.Fatal(err)
